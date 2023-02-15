e2b0c7b45ef5e2836ed69c7dcb2a4c3513091a8c
unknown <supaporn2275@gmail.com>
2023-02-15 14:19:27 -0800

    negative name - close #3

--- backend/entity/Employee.go
+++ backend/entity/Employee.go
@@ -3,7 +3,7 @@ package entity
 import "gorm.io/gorm"
 type Employee struct {
 	gorm.Model
-	Name string 		//`validator"required~Name not to be blank"`
+	Name string 		`valid:"required~Name not to be blank"`
 	Email string
-	EmployeeID string // รหัสพนักงานขึ7นต้นด้วย J หรือ M หรือ S แล้วตามด้วยตัวเลขจํานวน 8 ตัว
+	EmployeeID string // `valide`
 }
